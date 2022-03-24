113f9dfbd007d81e56e611246512dc414f81f02e
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2022-03-24 22:44:47 +0530

    refactor: lint

--- packages/react/index.js
+++ packages/react/index.js
@@ -1,12 +1,12 @@
 module.exports = {
-  extends: ["plugin:react/recommended", "@kolhe/eslint-config-ts"],
+  extends: ['plugin:react/recommended', '@kolhe/eslint-config-ts'],
   settings: {
     react: {
-      version: "17.0",
+      version: '17.0',
     },
   },
   rules: {
-    "jsx-quotes": ["error", "prefer-double"],
-    "react/react-in-jsx-scope": "off",
+    'jsx-quotes': ['error', 'prefer-double'],
+    'react/react-in-jsx-scope': 'off',
   },
-};
+}
