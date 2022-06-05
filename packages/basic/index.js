dce4f3230fd8a4a739c6ddc12872565649347b53
sumitkolhe <thesumitkolhe@gmail.com>
2022-06-05 14:52:34 +0530

    refactor: remove md rules

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -29,7 +29,7 @@ module.exports = defineConfig({
     '!.vitepress',
     '!.vscode',
   ],
-  plugins: ['html', 'unicorn', 'markdown'],
+  plugins: ['html', 'unicorn'],
   settings: {
     'import/resolver': {
       node: { extensions: ['.js', '.mjs'] },
@@ -106,23 +106,6 @@ module.exports = defineConfig({
       files: ['*.test.ts', '*.test.js', '*.spec.ts', '*.spec.js'],
       rules: {
         'no-unused-expressions': 'off',
-      },
-    },
-    {
-      // Code blocks in markdown file
-      files: ['**/*.md/*.*'],
-      rules: {
-        '@typescript-eslint/no-redeclare': 'off',
-        '@typescript-eslint/no-unused-vars': 'off',
-        '@typescript-eslint/no-use-before-define': 'off',
-        '@typescript-eslint/no-var-requires': 'off',
-        'import/no-unresolved': 'off',
-        'no-alert': 'off',
-        'no-console': 'off',
-        'no-restricted-imports': 'off',
-        'no-undef': 'off',
-        'no-unused-expressions': 'off',
-        'no-unused-vars': 'off',
       },
     },
   ],
