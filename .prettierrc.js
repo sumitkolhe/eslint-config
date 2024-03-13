33225eb582366697511cece6a005edf0a0238e77
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2024-03-13 17:15:12 +0530

    fix: prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,29 +1,4 @@
-/** @type {import('prettier').Options} */
-module.exports = {
+export default {
   semi: false,
   singleQuote: true,
-  trailingComma: "all",
-  overrides: [
-    {
-      files: [
-        "**/node_modules/**",
-        "**/dist/**",
-        "**/output/**",
-        "**/.changeset/**",
-        "**/.idea/**",
-        "**/.output/**",
-        "**/.vite-inspect/**",
-
-        "**/CHANGELOG*.md",
-        "**/*.min.*",
-        "**/LICENSE*",
-        "**/auto-import?(s).d.ts",
-        "**/components.d.ts",
-        "**/pnpm-lock.yaml",
-      ],
-      options: {
-        requirePragma: true,
-      },
-    },
-  ],
-};
+}
