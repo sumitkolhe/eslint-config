8a52da2eefadd3d78946994c9e3ec7b3dcba5169
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2024-03-15 11:50:37 +0530

    chore: updated deps

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import { createRequire } from 'node:module'
 
 const require = createRequire(import.meta.url)
 require('sucrase/register')
+
 /** @type {typeof import('./src/index.ts')} */
 const { config } = require('./src/index.ts')
 
