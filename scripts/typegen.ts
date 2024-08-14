7148b2208d58e6ecf2150db6455dee4e7ea7bf60
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2024-08-14 11:39:25 +0530

    feat: generate types for config

---
+++ scripts/typegen.ts
@@ -0,0 +1,21 @@
+import { writeFile } from 'node:fs/promises'
+import { flatConfigsToRulesDTS } from 'eslint-typegen/core'
+import { builtinRules } from 'eslint/use-at-your-own-risk'
+import pico from 'picocolors'
+import { config } from '../src/presets'
+
+const dts = await flatConfigsToRulesDTS(
+  config(
+    [
+      {
+        plugins: { '': { rules: Object.fromEntries(builtinRules.entries()) } },
+      },
+    ],
+    { vue: true, unocss: true },
+  ),
+  { includeAugmentation: false, exportTypeName: 'Rules' },
+)
+
+await writeFile('src/typegen.ts', dts)
+
+console.log(pico.green('Type definitions generated!'))
